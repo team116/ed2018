bcaa2b9718edb7807b2a1bb9eb6ac6ef51825a38
Rachel Fast <thebookworm2013@gmail.com>
2018-01-29 17:44:41 -0500

    Initial commit

---
+++ src/Subsystems/Vision.cpp
@@ -0,0 +1,18 @@
+/*
+ * Vision.cpp
+ *
+ *  Created on: Jan 29, 2018
+ *      Author: Steven
+ */
+
+#include "Vision.h"
+
+Vision::Vision() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Vision::~Vision() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Subsystems/Vision.h
@@ -0,0 +1,17 @@
+/*
+ * Vision.h
+ *
+ *  Created on: Jan 29, 2018
+ *      Author: Steven
+ */
+
+#ifndef SRC_SUBSYSTEMS_VISION_H_
+#define SRC_SUBSYSTEMS_VISION_H_
+
+class Vision {
+public:
+	Vision();
+	virtual ~Vision();
+};
+
+#endif /* SRC_SUBSYSTEMS_VISION_H_ */
